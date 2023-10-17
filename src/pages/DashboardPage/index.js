c462fadf3dad93bbdae02476bd6fe2c0ae8ed352
CJacobs <cjacobs3971@gmail.com>
2023-10-16 18:56:30 -0700

    improved design 2

--- src/pages/DashboardPage/index.js
+++ src/pages/DashboardPage/index.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import { useQuery } from '@apollo/client';
+import React, { useState, useEffect } from 'react';
+import { useQuery, useSubscription } from '@apollo/client';
 import gql from 'graphql-tag';
 import Post from '../../components/Shared/Post';
 import CategoryFilter from '../../components/Shared/CategoryFilter';
-import PostCreation from '../../components/Shared/PostCreation';  // Adjusted path
+import PostCreation from '../../components/Shared/PostCreation';
 import './style.css';
 
 const GET_POSTS = gql`
@@ -21,11 +21,32 @@ const GET_POSTS = gql`
   }
 `;
 
+const NEW_POST_SUBSCRIPTION = gql`
+  subscription {
+    newPost {
+      id
+      title
+      body
+      category
+      createdAt
+      author {
+        username
+      }
+    }
+  }
+`;
+
 function DashboardPage() {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const { loading, error, data } = useQuery(GET_POSTS);
+  const { data: subscriptionData } = useSubscription(NEW_POST_SUBSCRIPTION);
 
-  // Ensure data is loaded and data.getPosts is available
+  useEffect(() => {
+    if (subscriptionData && data && data.getPosts) {
+      data.getPosts.unshift(subscriptionData.newPost);
+    }
+  }, [subscriptionData, data]);
+
   let filteredPosts = [];
   if (data && data.getPosts) {
     filteredPosts = selectedCategory
@@ -37,27 +58,25 @@ function DashboardPage() {
   if (error) return <p>Error: {error.message}</p>;
 
   const handleLogout = () => {
-    localStorage.removeItem('token'); // Assuming you store the token in localStorage
-    // Redirect user to login or home page, or force a re-render
-    window.location.href = '/login-register'; // replace with your login page route
-  }
+    localStorage.removeItem('token');
+    window.location.href = '/login-register';
+  };
 
   return (
     <div className="dashboard-container">
       <div className="sidebar">
         <PostCreation />
         <CategoryFilter selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        
       </div>
       <div className="posts-section">
-        {filteredPosts.map(post => (
-          <Post key={post.id} post={post} />
-        ))}
-      </div>
+      {[...filteredPosts].reverse().map(post => (
+    <Post key={post.id} post={post} />
+    ))}
+</div>
       <div className="logoutsection">
-      <button onClick={handleLogout}>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
     </div>
-  </div>
   );
 }
 
